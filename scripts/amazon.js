80fd97f32b53e61651ec1001ec0b624321738fea
Lucasgfoli <lucasgabrielfer.oli@gmail.com>
2025-04-30 17:24:42 +0000

    cart features

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -59,9 +59,27 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid')
     .innerHTML = productsHTML;
 
-document.querySelector('.js-add-to-cart')
+document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
-      
+      const productName = button.dataset.productName;
+
+      let matchingItem;
+
+        cart.forEach((item) => {
+          if(productName === item.productName);
+            matchingItem = item;
+        });
+
+        if (matchingItem){
+          matchingItem.quantity += 1;
+        } else {
+          cart.push({
+            productName: productName,
+            quantity: 1
+          });
+        }
+
+        console.log(cart);
     });
   });
