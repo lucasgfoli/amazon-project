3d801ec770278fc824a0567bf73da8026afec523
Lucasgfoli <lucasgabrielfer.oli@gmail.com>
2025-05-02 13:11:02 +0000

    add cart quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -49,7 +49,7 @@ products.forEach((product) => {
           </div>
 
           <button class="add-to-cart-button button-primary js-add-to-cart" 
-          data-product-name = "${product.name}">
+          data-product-id = "${product.id}">
             Add to Cart
           </button>
         </div>
@@ -62,12 +62,12 @@ document.querySelector('.js-products-grid')
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
-      const productName = button.dataset.productName;
+      const productId = button.dataset.productId;
 
       let matchingItem;
 
         cart.forEach((item) => {
-          if(productName === item.productName);
+          if(productId === item.productId)
             matchingItem = item;
         });
 
@@ -75,11 +75,19 @@ document.querySelectorAll('.js-add-to-cart')
           matchingItem.quantity += 1;
         } else {
           cart.push({
-            productName: productName,
+            productId: productId,
             quantity: 1
           });
         }
+        
+        let cartQuantity = 0;
 
+        cart.forEach((item) => {
+
+          cartQuantity += item.quantity;
+        });
+
+        console.log(cartQuantity);
         console.log(cart);
     });
   });
